d07f3b2b63a820a29aeee2bad3a271541f05d441
HP <sy2000dn0@gmail.com>
2023-11-23 11:08:04 +0700

    delete note

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -77,7 +77,10 @@ dependencies {
     implementation ("androidx.room:room-ktx:2.2.5")
     implementation ("com.google.guava:guava:27.0.1-android")
     implementation("com.google.dagger:hilt-android:2.44")
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+//    implementation("com.google.dagger:hilt-android-compiler:2.44")
 }
 
 kapt {
--- app/src/main/java/com/example/note_app/presentation/nodes/NoteScreen.kt
+++ app/src/main/java/com/example/note_app/presentation/nodes/NoteScreen.kt
@@ -1,1 +1,69 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.note_app.presentation.nodes
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import kotlinx.coroutines.launch
+
+@Composable
+fun NoteScreen(navController: NavController,
+               noteViewModel: NotesViewModel = hiltViewModel()) {
+    val notes = noteViewModel.noteState.value
+    val scaffoldState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                content = { Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")})
+        },
+        snackbarHost = { SnackbarHost(scaffoldState)}
+    ) {
+        Spacer(
+            modifier = Modifier
+                .height(
+                    16.dp
+                )
+                .padding(it)
+        )
+        LazyColumn(modifier = Modifier.fillMaxHeight()){
+            items(notes) {
+                note -> NoteItem(note = note, modifier = Modifier.fillMaxWidth(),
+                    onDelete = {
+                        noteViewModel.onEvent(NoteEvent.DeleteNote(note))
+                        scope.launch {
+                            val result = scaffoldState.showSnackbar(
+                                message = "Note deleted",
+                                actionLabel = "Undo"
+                            )
+                            if (result == SnackbarResult.ActionPerformed) {
+                                noteViewModel.onEvent(NoteEvent.RestoreNote)
+                            }
+                        }
+                    })
+            }
+        }
+    }
+}
